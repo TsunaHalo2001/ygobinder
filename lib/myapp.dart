860e01ec6e59a47ea02deadb700c90ec6a5dc555
TsunaHalo2001 <joan.villamil@correounivalle.edu.co>
2025-10-29 16:18:52 -0500

    Initial commit

---
+++ lib/myapp.dart
@@ -0,0 +1,20 @@
+part of 'main.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'materialAppYGOBinder',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
---
+++ lib/myappstate.dart
@@ -0,0 +1,5 @@
+part of 'main.dart';
+
+class MyAppState extends ChangeNotifier {
+
+}
