0dc23d8afefb90255700aa3c347804dac26af059
TsunaHalo2001 <joan.villamil@correounivalle.edu.co>
2025-11-07 14:39:32 -0500

    feat: Implement native splash screens and loading UI
    This commit introduces native splash screens for iOS and Android and implements a new loading screen UI in Flutter.
    
    Key changes:
    - Add native splash screen configurations for Android, including styles for different API versions (`v31`, `night-v31`) and drawable assets.
    - Configure the iOS launch screen storyboard to use a background image, and add the corresponding image assets.
    - Create a `LoadingApp` widget in Flutter that displays a logo, a progress indicator, and a loading message.
    - Update `web/index.html` with styles and scripts for the web splash screen.
    - Add `assets/images/png/logo.png` to `pubspec.yaml`.
    - Upgrade `flutter_lints` and `lints` dependencies.

--- lib/loadingapp.dart
+++ lib/loadingapp.dart
@@ -3,10 +3,58 @@ part of 'main.dart';
 class LoadingApp extends StatelessWidget {
   const LoadingApp({super.key});
 
+  Future<void> loadData(BuildContext context) async {
+    //await context.read<YGOBinderState>();loadData();;
+
+    // Simulate a loading delay
+    await Future.delayed(const Duration(seconds: 2));
+
+    return;
+  }
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<YGOBinderState>();
+    final Size screenSize = MediaQuery.of(context).size;
 
-    return Placeholder();
+    return FutureBuilder(
+        future: loadData(context),
+        builder: (context, snapshot) {
+          return Scaffold(
+            backgroundColor: Color(0xFF4D2C6F),
+            body: Column(
+              children: [
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.only(top: screenSize.height * 0.2),
+                    child: Image.asset(
+                      'assets/images/png/logo.png',
+                      width: screenSize.height * 0.4,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: SizedBox(
+                    height: screenSize.height * 0.1,
+                  ),
+                ),
+                Center(child:
+                  // snapshot.connectionState == ConnectionState.done ?
+                    CircularProgressIndicator()
+                ),
+                Center(
+                  child: Text(
+                    'Obteniendo datos de la API...',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenSize.width > screenSize.height ?
+                        screenSize.height * 0.025 :
+                        screenSize.width * 0.05,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        });
   }
 }
--- lib/ygobinderstate.dart
+++ lib/ygobinderstate.dart
@@ -2,4 +2,6 @@ part of 'main.dart';
 
 class YGOBinderState extends ChangeNotifier {
   var state = 0;
+
+  // Future<void> loadData() async
 }
